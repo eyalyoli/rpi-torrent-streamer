3d2c78853ce9ac5e1f094f8503b748f70e7b1bf0
Eyal Abbas <eyal.abbas@retrain.ai>
2022-06-10 20:08:54 +0300

    add kill player

--- src/server/server.js
+++ src/server/server.js
@@ -59,16 +59,21 @@ app.get("/downloading", function(req, res) {
   });
 });
 
+var playerProc = null
 app.get("/stream/:hash", async function(req, res) {
   const hash = req.params.hash;
   console.log("stream=" + JSON.stringify(hash));
+
+  if(!playerProc.killed){
+    res.status(400).send("a player is already streaming...");
+  }
 
   //check if downloading
   const t = findTorrent(hash, currentTorrents);
   if (t) {
     await tc.getTorrentPath(hash, async (err, result) => {
-      if (err) res.status(500).send("error");
-      const playerProc = await cp.exec(
+      if (err) res.status(400).send("error");
+      playerProc = await cp.exec(
         cfg.server.PLAYER_CMD + ' "' + result.path + '"'
       );
       console.log("running player at " + playerProc.pid);
@@ -80,4 +85,10 @@ app.get("/stream/:hash", async function(req, res) {
       .send("torrent was not found. please download it before you stream.");
   }
 });
+
+app.get("/streamkill", async function(req, res){
+  console.log('killing player...')
+  playerProc.kill('SIGINT')
+  res.send('OK')
+})
 // <<<<<<<<<<<<<<<<< Routing <<<<<<<<<<<<<<<<<
