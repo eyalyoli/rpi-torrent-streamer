376a77de3bd622382313f7d8cd691736e9094b96
Eyal Abbas <eyal.abbas@retrain.ai>
2022-08-03 19:59:40 +0300

    add nvm installation

--- install_server.sh
+++ install_server.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 # install & config transmission torrent client + vlc 
-echo -n 'Installing torrent client & VLC...'
+echo 'Installing torrent client & VLC...'
 sudo apt update 
 sudo apt install -y transmission-cli transmission-common transmission-daemon 
 sudo apt install vlc
-echo ' Done'
+echo 'Done'
 
-echo -n 'Configuring torrent client...'
+echo 'Configuring torrent client...'
 sudo service transmission-daemon stop 
 sudo cp transmission-settings.json /root/.config/transmission-daemon/settings.json
 sudo service transmission-daemon start 
-echo ' Done'
+echo 'Done'
 
 # install & build source code
 
-echo -n 'Configuring torrent client...'
+echo 'Installing nvm + npm + node...'
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+export NVM_DIR="/home/pi/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install node
+echo 'Done'
+
+echo 'Configuring torrent client...'
 npm install
 npm run build
-echo ' Done'
+echo 'Done'
 
 echo '\nYou can now run the server!'
